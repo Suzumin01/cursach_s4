cb2b21651c368f314508c39fb4180ed24eb97563
Nikita Smolnikov <151205132+Suzumin01@users.noreply.github.com>
2024-06-03 03:51:34 +0300

    добавлено правильное давление, округление средних результатов и прочее

--- app/src/main/java/com/example/mhhp/HomeFragment.java
+++ app/src/main/java/com/example/mhhp/HomeFragment.java
@@ -13,22 +13,31 @@ import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
-    private TextView greetingTextView;
+    private TextView greetingTextView, averageWeightTextView, averageBloodPressureTextView;
+    private DatabaseHelper databaseHelper;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
         greetingTextView = view.findViewById(R.id.greetingTextView);
+        averageWeightTextView = view.findViewById(R.id.averageWeightTextView);
+        averageBloodPressureTextView = view.findViewById(R.id.averageBloodPressureTextView);
+        databaseHelper = new DatabaseHelper(getActivity());
 
         // Загрузка имени и отчества пользователя из SharedPreferences
         SharedPreferences preferences = getActivity().getSharedPreferences("userProfile", Context.MODE_PRIVATE);
         String firstName = preferences.getString("firstName", "User");
         String middleName = preferences.getString("middleName", "");
-
         greetingTextView.setText("Здравствуйте, " + firstName + " " + middleName + "!");
 
-        // Здесь добавить логику для вычисления и отображения средних показателей
+        // Вычисление и отображение средних показателей
+        double averageWeight = databaseHelper.getAverageWeight();
+        String averageBloodPressure = databaseHelper.getAverageBloodPressure();
+
+        averageWeightTextView.setText("Average Weight: " + averageWeight);
+        averageBloodPressureTextView.setText("Average Blood Pressure: " + averageBloodPressure);
 
         return view;
     }
